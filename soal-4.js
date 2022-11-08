74fe94e2435689d2c18951080dfcc3360db5ec24
Rio Prayoga <yoprayogap@gmail.com>
2022-11-08 11:34:21 +0700

    mengganti flowchart

--- soal-4.js
+++ soal-4.js
@@ -1,38 +1,15 @@
-//! cara pertama
-const palindrome = (word) => {
-  let isPalindrome = "";
-
-  if (word.length > 2) {
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] == word[word.length - (i + 1)]) {
-        isPalindrome += word[i];
-      } else {
-        return "Not Palindrome";
-      }
-    }
-    return `${isPalindrome} is palindrome`;
-  } else {
-    return "Letters must be more than 2 characters";
-  }
-};
-
-//! cara kedua
 const palindrome2 = (word) => {
-  let isPalindrome = "";
+  let result = "";
 
   if (word.length > 2) {
     for (let i = 0; i < word.length; i++) {
-      isPalindrome += word[word.length - (i + 1)];
+      result += word[word.length - (i + 1)];
     }
   } else {
     return "Letters must be more than 2 characters";
   }
 
-  const result =
-    word == isPalindrome ? `${isPalindrome} is Palindrome` : `Not palindrome`;
-
-  return result;
+  return word == result ? `${result} is Palindrome` : `Not palindrome`;
 };
 
-console.log(palindrome("malam"));
-console.log(palindrome2("katak"));
+console.log(palindrome2("ada"));
